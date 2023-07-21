7af06ce3db9ababaeb0ed2e7ce7d4a6e85e7b00d
jorgectf <jorgectf@github.com>
2023-07-21 14:09:11 +0200

    Remove `node_modules`

--- jupyterlab-codeql-highlight/node_modules/@blueprintjs/core/lib/cjs/components/hotkeys/index.d.ts
+++
@@ -1,9 +0,0 @@
-export * from "./hotkeysTypes";
-export * from "./hotkeys";
-export { Hotkey, IHotkeyProps } from "./hotkey";
-export { KeyCombo, KeyComboTagProps, IKeyComboProps } from "./keyCombo";
-export { HotkeysTarget, IHotkeysTargetComponent } from "./hotkeysTarget";
-export { IKeyCombo, comboMatches, getKeyCombo, getKeyComboString, parseKeyCombo } from "./hotkeyParser";
-export { IHotkeysDialogProps, hideHotkeysDialog, setHotkeysDialogProps } from "./hotkeysDialog";
-export { HotkeysDialog2 } from "./hotkeysDialog2";
-export { HotkeysTarget2, HotkeysTarget2Props } from "./hotkeysTarget2";
